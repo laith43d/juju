0ffaa35630b41fa3b6f1d244d9148427268b3f81
LZAH <laith43d@gmail.com>
2018-04-25 02:02:40 +0300

    major dir restructure. recreated db migrations including sqlalchemy and excluding orator for lack of support.

--- facilities/decorators.py
+++ facilities/decorators.py
@@ -1,9 +1,25 @@
 from functools import wraps
 
-from flask import Response, redirect, request
-from flask_orator import jsonify
+from flask import Response, jsonify, redirect, request
 
 from .helpers import json_value
+from .response import make_json_response
+
+
+def jsonapi(f):
+    """ Declare the view as a JSON API method
+        This converts view return value into a :cls:JsonResponse.
+        The following return types are supported:
+            - tuple: a tuple of (response, status, headers)
+            - any other object is converted to JSON
+    """
+
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        rv = f(*args, **kwargs)
+        return make_json_response(rv)
+
+    return wrapper
 
 
 def ssl_required(f):
@@ -49,6 +65,6 @@ def as_json(f):
         if isinstance(response, dict):
             return jsonify(**response)
         else:
-            return jsonify(result=response)
+            return jsonify(result = response)
 
     return wrapper
