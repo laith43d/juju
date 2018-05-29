6392bac9e73b1e0a464edadd16a9dca2a973d84e
LZAH <laith43d@gmail.com>
2018-05-29 19:01:37 +0300

    api command bugfix

--- cli/commands/cmd_api.py
+++ cli/commands/cmd_api.py
@@ -32,7 +32,7 @@ def make_api(api_name: str, model):
         with open(f'api/{api_name.capitalize()}.py', 'w') as f:
             f.write(f"from flask_classful import FlaskView as Resource, route\n"
                     f"from models.{api_name.capitalize()} import {api_name.capitalize()}\n"
-                    f"from flask_orator import jsonify"
+                    f"from flask_orator import jsonify\n"
                     f"from flask import request\n"
                     f"\n"
                     f"class {api_name.capitalize()}View(Resource):\n"
