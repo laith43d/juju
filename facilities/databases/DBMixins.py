0f644faee42a074c33fa96a7471fd354779cc516
LZAH <laith43d@gmail.com>
2018-06-01 07:06:20 +0300

    changed event lister for updated_at.

--- facilities/databases/DBMixins.py
+++ facilities/databases/DBMixins.py
@@ -1,26 +1,18 @@
 import datetime
 
-from sqlalchemy import Column, DateTime, Integer, event, inspect
+from sqlalchemy import Column, DateTime, Integer, inspect
 
 
 class Timestamp():
-    """Adds `created_at` and `updated` columns to a derived declarative model.
+    """Adds `created_at` and `updated_at` columns to a derived declarative model.
 
     The `created_at` column is handled through a default and the `updated_at`
-    column is handled through a `before_update` event that propagates
-    for all derived declarative models.
+    column is handled through a onupdate event trigger.
 
     """
 
     created_at = Column(DateTime, default = datetime.datetime.now())
     updated_at = Column(DateTime, onupdate = datetime.datetime.utcnow())
-
-
-@event.listens_for(Timestamp, 'before_update', propagate = True)
-def timestamp_before_update(target):
-    # When a model with a timestamp is updated; force update the updated
-    # timestamp.
-    target.updated_at = datetime.datetime.now()
 
 
 class IDMixin(Timestamp):
