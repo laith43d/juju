8ea53946b878e79c186465eb6b7ef5f9a39ce23a
LZAH <laith43d@gmail.com>
2018-06-17 18:59:37 +0300

    created a universal app instance.

--- api/__init__.py
+++ api/__init__.py
@@ -2,6 +2,3 @@ from config.settings import create_app
 from config.static_config import API_PREFIX
 app = create_app()
 
-
-
-
--- app.py
+++ app.py
@@ -1,7 +1,4 @@
-from config.settings import create_app
-
-application = app = create_app()
-
+from config.settings import app
 
 if __name__ == '__main__':
     app.run()
